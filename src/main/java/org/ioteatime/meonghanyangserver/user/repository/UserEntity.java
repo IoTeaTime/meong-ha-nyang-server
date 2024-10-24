e45e91babe73289fae7892eb9cbc5e5719acc323
OziinG <dlajiin@gmail.com>
2024-10-24 11:05:12 +0900

    feat: User 도메인 추가 (#16)

---
+++ src/main/java/org/ioteatime/meonghanyangserver/user/repository/UserEntity.java
@@ -0,0 +1,27 @@
+package org.ioteatime.meonghanyangserver.user.repository;
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String profile_img_url;
+}
