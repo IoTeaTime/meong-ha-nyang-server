bf3eaa09ebb46514ab8e3560a05176019b76b924
OziinG <dlajiin@gmail.com>
2024-10-24 17:37:06 +0900

    fix: SecurityConfig 수정 (#24)

--- src/main/java/org/ioteatime/meonghanyangserver/config/SecurityConfig.java
+++ src/main/java/org/ioteatime/meonghanyangserver/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.ioteatime.meonghanyangserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,8 +11,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).authorizeRequests().anyRequest().permitAll(); // 모든 요청 허용
-
+        http.csrf(CsrfConfigurer::disable);
         return http.build();
     }
 }
