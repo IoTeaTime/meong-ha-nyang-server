b93c4f6782eac00b12f18ab48faa308ae5fe218e
OziinG <dlajiin@gmail.com>
2024-10-24 13:32:26 +0900

    refactor: 도메인 디렉토리 분리 (#16)

---
+++ src/main/java/org/ioteatime/meonghanyangserver/user/domain/UserEntity.java
@@ -0,0 +1,25 @@
+package org.ioteatime.meonghanyangserver.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column private String profileImgUrl;
+}
