92dfdece5b3bf721b4d5b2c41777b27ecdc39cca
OziinG <dlajiin@gmail.com>
2024-10-25 09:17:50 +0900

    fix: UserDetailResponse 레코드로 변경 (#24)

--- src/main/java/org/ioteatime/meonghanyangserver/user/dto/response/UserDetailResponse.java
+++ src/main/java/org/ioteatime/meonghanyangserver/user/dto/response/UserDetailResponse.java
@@ -1,18 +1,22 @@
 package org.ioteatime.meonghanyangserver.user.dto.response;
 
 import lombok.Getter;
+import org.ioteatime.meonghanyangserver.user.domain.UserEntity;
 
 @Getter
-public class UserDetailResponse {
-    private Long id;
-    private String email;
-    private String profileImgUrl;
-    private String nickname;
-
-    public UserDetailResponse(Long id, String email, String profileImgUrl, String nickname) {
-        this.id = id;
-        this.email = email;
-        this.profileImgUrl = profileImgUrl;
-        this.nickname = nickname;
+public record UserDetailResponse(
+        Long id,
+        String email,
+        String profileImgUrl,
+        String nickname
+) {
+    // UserEntity -> UserDetailResponse로 변환하는 정적 메서드
+    public static UserDetailResponse from(UserEntity user) {
+        return new UserDetailResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getProfileImgUrl(),
+                user.getNickname()
+        );
     }
 }
