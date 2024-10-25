e7b7ff760ce1230b3c89b2fb77b675ac927c9e6e
OziinG <dlajiin@gmail.com>
2024-10-25 10:01:08 +0900

    fix: 예외처리 분리 (#24) (KAN-77)

--- src/main/java/org/ioteatime/meonghanyangserver/user/dto/response/UserDetailResponse.java
+++ src/main/java/org/ioteatime/meonghanyangserver/user/dto/response/UserDetailResponse.java
@@ -1,22 +1,15 @@
 package org.ioteatime.meonghanyangserver.user.dto.response;
 
-import lombok.Getter;
+import java.util.Optional;
 import org.ioteatime.meonghanyangserver.user.domain.UserEntity;
 
-@Getter
-public record UserDetailResponse(
-        Long id,
-        String email,
-        String profileImgUrl,
-        String nickname
-) {
-    // UserEntity -> UserDetailResponse로 변환하는 정적 메서드
-    public static UserDetailResponse from(UserEntity user) {
+
+public record UserDetailResponse(Long id, String email, String profileImgUrl, String nickname) {
+    public static UserDetailResponse from(Optional<UserEntity> optionalUser) {
+        UserEntity user =
+                optionalUser.orElseThrow(() -> new IllegalArgumentException("User not found"));
+
         return new UserDetailResponse(
-                user.getId(),
-                user.getEmail(),
-                user.getProfileImgUrl(),
-                user.getNickname()
-        );
+                user.getId(), user.getNickname(), user.getEmail(), user.getProfileImgUrl());
     }
 }
--- src/main/java/org/ioteatime/meonghanyangserver/user/service/UserService.java
+++ src/main/java/org/ioteatime/meonghanyangserver/user/service/UserService.java
@@ -1,5 +1,6 @@
 package org.ioteatime.meonghanyangserver.user.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.ioteatime.meonghanyangserver.user.domain.UserEntity;
 import org.ioteatime.meonghanyangserver.user.dto.response.UserDetailResponse;
@@ -12,18 +13,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserDetailResponse getUserDetail(Long userId) {
-        UserEntity userEntity =
-                userRepository
-                        .findById(userId)
-                        .orElseThrow(
-                                () ->
-                                        new IllegalArgumentException(
-                                                "User not found with ID: " + userId));
-
-        return new UserDetailResponse(
-                userEntity.getId(),
-                userEntity.getEmail(),
-                userEntity.getProfileImgUrl(),
-                userEntity.getNickname());
+        Optional<UserEntity> userEntity = userRepository.findById(userId);
+        return UserDetailResponse.from(userEntity);
     }
 }
