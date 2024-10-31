703b885f6be35d138272f169f520b7f3fe0521df
OziinG <dlajiin@gmail.com>
2024-10-31 13:49:39 +0900

    fix: Spotless 수정 (#44) (KAN-100)

--- src/main/java/org/ioteatime/meonghanyangserver/cctv/dto/response/CctvInviteResponse.java
+++ src/main/java/org/ioteatime/meonghanyangserver/cctv/dto/response/CctvInviteResponse.java
@@ -6,5 +6,8 @@ import jakarta.validation.constraints.NotNull;
 @Schema(description = "CCTV 초대 응답")
 public record CctvInviteResponse(
         @Schema(description = "그룹 ID", example = "1") Long groupId,
-        @NotNull @Schema(description = "KVS 채널 이름", example = "kvs-9bc02f62-df22-40b0-bdc1-610d8fd293b4")
+        @NotNull
+                @Schema(
+                        description = "KVS 채널 이름",
+                        example = "kvs-9bc02f62-df22-40b0-bdc1-610d8fd293b4")
                 String kvsChannelId) {}
