ce0dd25a315f67a1ea467c2c5654fd9dd800a513
OziinG <dlajiin@gmail.com>
2024-10-31 13:48:51 +0900

    fix: CCTV 초대 응답 Swagger 수정 (#44) (KAN-100)

--- src/main/java/org/ioteatime/meonghanyangserver/cctv/dto/response/CctvInviteResponse.java
+++ src/main/java/org/ioteatime/meonghanyangserver/cctv/dto/response/CctvInviteResponse.java
@@ -1,9 +1,10 @@
 package org.ioteatime.meonghanyangserver.cctv.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "CCTV 초대 응답")
 public record CctvInviteResponse(
         @Schema(description = "그룹 ID", example = "1") Long groupId,
-        @Schema(description = "KVS 채널 이름", example = "kvs-9bc02f62-df22-40b0-bdc1-610d8fd293b4")
+        @NotNull @Schema(description = "KVS 채널 이름", example = "kvs-9bc02f62-df22-40b0-bdc1-610d8fd293b4")
                 String kvsChannelId) {}
