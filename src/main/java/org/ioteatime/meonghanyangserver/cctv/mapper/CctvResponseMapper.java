be74116bf4236abf7b3f5e3f83747055ac9dd45a
민석 <47300870+seok019283501@users.noreply.github.com>
2024-11-01 10:48:41 +0900

    fix: cctv info response의 cctv nickname 중복 해결 (#47) (KAN-102)

--- src/main/java/org/ioteatime/meonghanyangserver/cctv/mapper/CctvResponseMapper.java
+++ src/main/java/org/ioteatime/meonghanyangserver/cctv/mapper/CctvResponseMapper.java
@@ -6,6 +6,6 @@ import org.ioteatime.meonghanyangserver.cctv.dto.response.CctvInfoResponse;
 public class CctvResponseMapper {
     public static CctvInfoResponse toCctvInfoResponse(CctvEntity cctvEntity) {
         return new CctvInfoResponse(
-                cctvEntity.getId(), cctvEntity.getCctvNickname(), cctvEntity.getCctvNickname());
+                cctvEntity.getId(), cctvEntity.getCctvNickname(), cctvEntity.getKvsChannelName());
     }
 }
