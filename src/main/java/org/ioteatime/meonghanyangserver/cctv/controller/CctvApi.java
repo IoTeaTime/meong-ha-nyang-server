a224a5b3c1b0f54f9465aa9ccf4c735a6fa61d41
ywonchae1 <ywonchae62@gmail.com>
2024-11-03 13:13:14 +0900

    refactor: Swagger 담당자 description 추가 (#70) (KAN-128)

---
+++ src/main/java/org/ioteatime/meonghanyangserver/cctv/controller/CctvApi.java
@@ -0,0 +1,13 @@
+package org.ioteatime.meonghanyangserver.cctv.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.ioteatime.meonghanyangserver.common.api.Api;
+import org.ioteatime.meonghanyangserver.common.utils.LoginMember;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Tag(name = "CCTV Api", description = "CCTV 관련 API 목록입니다.")
+public interface CctvApi {
+    @Operation(summary = "CCTV 삭제(퇴출)", description = "담당자: 양원채")
+    Api<?> delete(@LoginMember Long userId, @PathVariable Long cctvId);
+}
