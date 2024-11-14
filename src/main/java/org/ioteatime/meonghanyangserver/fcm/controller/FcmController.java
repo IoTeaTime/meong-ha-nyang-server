c7ddc64e9b5250c32466daf53698e668c0abc5b3
ywonchae1 <ywonchae62@gmail.com>
2024-11-14 16:44:19 +0900

    feat: Fcm 토큰 저장 구현 (#115)

--- src/main/java/org/ioteatime/meonghanyangserver/fcm/controller/FcmController.java
+++ src/main/java/org/ioteatime/meonghanyangserver/fcm/controller/FcmController.java
@@ -3,21 +3,24 @@ package org.ioteatime.meonghanyangserver.fcm.controller;
 import lombok.RequiredArgsConstructor;
 import org.ioteatime.meonghanyangserver.common.api.Api;
 import org.ioteatime.meonghanyangserver.common.type.FcmSuccessType;
+import org.ioteatime.meonghanyangserver.common.utils.LoginMember;
 import org.ioteatime.meonghanyangserver.fcm.dto.request.SaveFcmTokenRequest;
 import org.ioteatime.meonghanyangserver.fcm.service.FcmService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/open-api/fcm") // 테스트 후 api로 변경하기
+@RequestMapping("/api/fcm") // 테스트 후 api로 변경하기
 public class FcmController implements FcmApi {
     private final FcmService fcmService;
 
     @PostMapping("/token")
-    public Api<?> saveToken(SaveFcmTokenRequest createFcmTokenRequest) {
-        fcmService.saveToken(createFcmTokenRequest.token());
+    public Api<?> saveToken(
+            @LoginMember Long memberId, @RequestBody SaveFcmTokenRequest createFcmTokenRequest) {
+        fcmService.saveToken(memberId, createFcmTokenRequest.token());
         return Api.success(FcmSuccessType.SAVE);
     }
 }
