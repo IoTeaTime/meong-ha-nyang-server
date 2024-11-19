c95f2c723a5e53168ad99b42bcd9075cf670e8c4
민석 <47300870+seok019283501@users.noreply.github.com>
2024-11-19 18:19:20 +0900

    fix: 에러코드 고정 해결 (#132)

--- src/main/java/org/ioteatime/meonghanyangserver/handler/ApiExceptionHandler.java
+++ src/main/java/org/ioteatime/meonghanyangserver/handler/ApiExceptionHandler.java
@@ -18,8 +18,7 @@ public class ApiExceptionHandler {
     @ExceptionHandler(value = ApiExceptionImpl.class)
     public ResponseEntity<Api<Object>> apiResponseEntity(ApiExceptionImpl apiExceptionImpl) {
         log.debug("", apiExceptionImpl);
-        ErrorTypeCode errorTypeCode = apiExceptionImpl.getErrorTypeCode();
         return ResponseEntity.status(apiExceptionImpl.getHttpStatus())
-                .body(Api.fail(errorTypeCode));
+                .body(Api.fail(apiExceptionImpl));
     }
 }
