51c6739077e5290f4da3403d2e639b4a091f5dbc
ywonchae1 <ywonchae62@gmail.com>
2024-11-21 15:39:55 +0900

    feat: 환경변수 객체화 (#147)

--- src/main/java/org/ioteatime/meonghanyangserver/MeongHaNyangServerApplication.java
+++ src/main/java/org/ioteatime/meonghanyangserver/MeongHaNyangServerApplication.java
@@ -2,10 +2,12 @@ package org.ioteatime.meonghanyangserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MeongHaNyangServerApplication {
 
     public static void main(String[] args) {
