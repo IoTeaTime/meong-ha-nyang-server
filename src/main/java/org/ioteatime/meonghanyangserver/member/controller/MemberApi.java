b0fc998efbca3625f74890b60e82d095a5953df9
ywonchae1 <ywonchae62@gmail.com>
2024-11-22 18:50:17 +0900

    docs: API 설명 추가 (#157)

--- src/main/java/org/ioteatime/meonghanyangserver/member/controller/MemberApi.java
+++ src/main/java/org/ioteatime/meonghanyangserver/member/controller/MemberApi.java
@@ -31,7 +31,8 @@ public interface MemberApi {
 
     @Operation(
             summary = "회원 닉네임과 회원이 소속된 그룹명을 변경합니다.",
-            description = "담당자: 양원채\n\n닉네임 또는 그룹명을 갱신할 수 있으며, 한꺼번에 갱신도 가능합니다. 응답으로는 변경된 사항만 응답합니다.")
+            description =
+                    "담당자: 양원채\n\n닉네임 또는 그룹명을 갱신할 수 있으며, 한꺼번에 갱신도 가능합니다.\n\n갱신하고 싶지 않은 필드는 아예 적지 않거나 null로 담아 요청하시면 됩니다. 응답으로는 변경된 사항만 응답합니다.")
     Api<UpdateNicknameAndGroupNameResponse> updateNicknameAndGroupName(
             @LoginMember Long memberId, @RequestBody UpdateNicknameAndGroupNameRequest request);
 }
