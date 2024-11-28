6e6de5efcaee53a3a88a241ea0c1116ed859b34a
ywonchae1 <ywonchae62@gmail.com>
2024-11-28 11:40:08 +0900

    fix: Video thumbnail entity 제거 (#190)

--- src/main/java/org/ioteatime/meonghanyangserver/videothumbnail/repository/VideoThumbnailRepository.java
+++
@@ -1,6 +0,0 @@
-package org.ioteatime.meonghanyangserver.videothumbnail.repository;
-
-import org.ioteatime.meonghanyangserver.videothumbnail.domain.VideoThumbnailEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoThumbnailRepository extends JpaRepository<VideoThumbnailEntity, Long> {}
