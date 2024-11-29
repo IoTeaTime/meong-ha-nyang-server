4bd31e05f93811bb958b48718d4a9c679f720bd6
ywonchae1 <ywonchae62@gmail.com>
2024-11-29 17:34:24 +0900

    feat: KVS 채널 삭제 시 shadow 업데이트할 수 있는 객체 추가 (#211)

--- src/main/java/org/ioteatime/meonghanyangserver/clients/iot/IotShadowMqttClient.java
+++ src/main/java/org/ioteatime/meonghanyangserver/clients/iot/IotShadowMqttClient.java
@@ -1,6 +1,9 @@
 package org.ioteatime.meonghanyangserver.clients.iot;
 
+import java.util.HashMap;
 import lombok.RequiredArgsConstructor;
+import org.ioteatime.meonghanyangserver.common.exception.InternalServerException;
+import org.ioteatime.meonghanyangserver.common.type.IoTErrorType;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.crt.mqtt.QualityOfService;
 import software.amazon.awssdk.iot.iotshadow.IotShadowClient;
@@ -13,15 +16,21 @@ public class IotShadowMqttClient {
     private final IotShadowClient iotShadowClient;
 
     public void updateShadow(String thingName, String key, boolean value) {
-        ShadowState shadowState = new ShadowState();
-        shadowState.desired.put(key, value);
-        shadowState.reportedIsNullable = true;
+        try {
+            ShadowState shadowState = new ShadowState();
+            shadowState.desired = new HashMap<>();
+            shadowState.desired.put(key, value);
+            shadowState.reportedIsNullable = true;
 
-        UpdateShadowRequest updateShadowRequest = new UpdateShadowRequest();
-        updateShadowRequest.state = shadowState;
+            UpdateShadowRequest updateShadowRequest = new UpdateShadowRequest();
+            updateShadowRequest.state = shadowState;
 
-        updateShadowRequest.thingName = thingName;
+            updateShadowRequest.thingName = thingName;
 
-        iotShadowClient.PublishUpdateShadow(updateShadowRequest, QualityOfService.AT_LEAST_ONCE);
+            iotShadowClient.PublishUpdateShadow(
+                    updateShadowRequest, QualityOfService.AT_LEAST_ONCE);
+        } catch (Exception e) {
+            throw new InternalServerException(IoTErrorType.UPDATE_SHADOW);
+        }
     }
 }
