5f10890fa67d43f0bbbf26140502eba80365b69b
ywonchae1 <ywonchae62@gmail.com>
2024-12-01 19:43:07 +0900

    feat: CCTV 삭제 테스트코드에 KVS 채널 생성 부분 추가, 예외 처리 테스트코드 구현 (#231)

--- src/test/java/org/ioteatime/meonghanyangserver/cctv/controller/CctvControllerTest.java
+++ src/test/java/org/ioteatime/meonghanyangserver/cctv/controller/CctvControllerTest.java
@@ -1,6 +1,6 @@
 package org.ioteatime.meonghanyangserver.cctv.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -8,6 +8,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.assertj.core.api.Assertions;
 import org.ioteatime.meonghanyangserver.cctv.domain.CctvEntity;
 import org.ioteatime.meonghanyangserver.cctv.repository.CctvRepository;
+import org.ioteatime.meonghanyangserver.clients.kvs.KvsClient;
+import org.ioteatime.meonghanyangserver.common.type.CommonErrorType;
 import org.ioteatime.meonghanyangserver.common.utils.JwtUtils;
 import org.ioteatime.meonghanyangserver.config.ControllerTestConfig;
 import org.ioteatime.meonghanyangserver.group.domain.GroupEntity;
@@ -28,6 +30,7 @@ import org.springframework.http.MediaType;
 @SpringBootTest
 class CctvControllerTest extends ControllerTestConfig {
     @Autowired private JwtUtils jwtUtils;
+    @Autowired private KvsClient kvsClient;
     @Autowired private MemberRepository memberRepository;
     @Autowired private CctvRepository cctvRepository;
     @Autowired private GroupRepository groupRepository;
@@ -54,7 +57,8 @@ class CctvControllerTest extends ControllerTestConfig {
 
     @Test
     void CCTV를_삭제합니다() throws Exception {
-        GroupEntity group = GroupEntity.builder().groupName("testgroup").build();
+        GroupEntity group =
+                GroupEntity.builder().groupName("testgroup").fcmTopic("testTopic").build();
         groupRepository.save(group);
 
         GroupMemberEntity groupMember =
@@ -66,17 +70,19 @@ class CctvControllerTest extends ControllerTestConfig {
                         .build();
         groupMemberRepository.save(groupMember);
 
+        String kvsChannelName = "test-delete-channel";
+
         CctvEntity cctv =
                 CctvEntity.builder()
                         .group(group)
                         .cctvNickname("test")
-                        .kvsChannelName("test-delete-channel") // KVS에 채널이 존재해야 함
+                        .kvsChannelName(kvsChannelName) // KVS에 채널이 존재해야 함
                         .thingId("test-thing-id")
                         .build();
 
         cctvRepository.save(cctv);
 
-        String kvsChannelName = cctv.getKvsChannelName(); // KVS에 채널이 존재해야 함
+        kvsClient.createSignalingChannel(kvsChannelName); // KVS 채널 생성
 
         mockMvc.perform(
                         delete("/api/cctv/{cctvId}/out", cctv.getId())
@@ -90,5 +96,15 @@ class CctvControllerTest extends ControllerTestConfig {
         Assertions.assertThat(cctvRepository.existsByKvsChannelName(kvsChannelName))
                 .isEqualTo(false);
         Assertions.assertThat(cctvRepository.findById(cctv.getId())).isEmpty();
+    }
+
+    @Test
+    void 글로벌_예외_처리를_테스트합니다() throws Exception {
+        // 일치하는 요청 경로가 없는 경우
+        mockMvc.perform(get("/open-api/cctv/hello-world"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(
+                        jsonPath("$.result.description")
+                                .value(CommonErrorType.ILLEGAL_ARGUMENT.getDescription()));
     }
 }
