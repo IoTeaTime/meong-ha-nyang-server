9721e2784a5608ae0f37858235c35c3c1e40fae9
ywonchae1 <ywonchae62@gmail.com>
2024-12-04 14:38:11 +0900

    docs: API 문서 설명 추가 (#243)

--- src/main/java/org/ioteatime/meonghanyangserver/image/controller/ImageApi.java
+++ src/main/java/org/ioteatime/meonghanyangserver/image/controller/ImageApi.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Tag(name = "Image Api", description = "Image 관련 API 목록입니다.")
 public interface ImageApi {
     @Operation(
-            summary = "회원이 속한 그룹의 날짜별 객체 탐지 이미지 목록을 시간 순서대로 조회합니다.",
+            summary = "회원이 속한 그룹의 날짜별 객체 탐지 이미지 목록을 최근 시간 순서대로 조회합니다.",
             description =
                     "담당자: 양원채\n\n달력의 날짜를 클릭했을 때 해당 날짜의 이미지 목록을 조회할 떄 사용합니다.\n\nURL QueryParameter로 날짜를 담아 요청 주시면 됩니다.")
     Api<GroupDateImageResponse> groupDateImageList(
