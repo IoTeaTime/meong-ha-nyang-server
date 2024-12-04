7b7d54eae3ed6dc96113cb9148125e9244da6778
OziinG <dlajiin@gmail.com>
2024-12-04 16:13:44 +0900

    fix: 코드 오류 수정 (#242)

--- src/main/java/org/ioteatime/meonghanyangserver/image/controller/ImageController.java
+++ src/main/java/org/ioteatime/meonghanyangserver/image/controller/ImageController.java
@@ -5,7 +5,6 @@ import org.ioteatime.meonghanyangserver.common.api.Api;
 import org.ioteatime.meonghanyangserver.common.type.ImageSuccessType;
 import org.ioteatime.meonghanyangserver.common.utils.LoginMember;
 import org.ioteatime.meonghanyangserver.image.dto.response.GroupDateImageResponse;
-import org.ioteatime.meonghanyangserver.image.dto.response.ImageSaveUrlResponse;
 import org.ioteatime.meonghanyangserver.image.service.ImageService;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,12 +23,5 @@ public class ImageController implements ImageApi {
         GroupDateImageResponse response =
                 imageService.findAllByMemberIdAndDate(memberId, year, month, day);
         return Api.success(ImageSuccessType.GET_LIST_OF_DATE, response);
-    }
-
-    @GetMapping("/{fileName}")
-    public Api<ImageSaveUrlResponse> getImageSaveUrl(
-            @LoginMember Long cctvId, @PathVariable String fileName) {
-        ImageSaveUrlResponse imageSaveUrlResponse = imageService.getImageSaveUrl(cctvId, fileName);
-        return Api.success(ImageSuccessType.CREATE_PRESIGNED_URL, imageSaveUrlResponse);
     }
 }
