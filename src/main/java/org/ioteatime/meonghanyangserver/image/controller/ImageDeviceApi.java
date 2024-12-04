769671f0e30d5116eb4acff87720fce291c9a130
ywonchae1 <ywonchae62@gmail.com>
2024-12-04 18:34:23 +0900

    fix: 이미지 Presigned URL 발급 메서드 변경 (#247)

--- src/main/java/org/ioteatime/meonghanyangserver/image/controller/ImageDeviceApi.java
+++ src/main/java/org/ioteatime/meonghanyangserver/image/controller/ImageDeviceApi.java
@@ -4,8 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.ioteatime.meonghanyangserver.common.api.Api;
 import org.ioteatime.meonghanyangserver.common.utils.LoginMember;
+import org.ioteatime.meonghanyangserver.image.dto.request.ImageNameRequest;
 import org.ioteatime.meonghanyangserver.image.dto.response.ImageSaveUrlResponse;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Image Api", description = "Image 관련 API 목록입니다.")
 public interface ImageDeviceApi {
@@ -14,5 +15,5 @@ public interface ImageDeviceApi {
             description =
                     "담당자: 임지인\n\n캡쳐 이미지 저장을 위한 URL 발급 요청입니다.\n\nCCTV의 Access Token과 함께 요청 하시면 됩니다.")
     Api<ImageSaveUrlResponse> getImageSaveUrl(
-            @LoginMember Long cctvId, @PathVariable String fileName);
+            @LoginMember Long cctvId, @RequestBody ImageNameRequest imageNameRequest);
 }
